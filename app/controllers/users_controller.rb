490fec623a79ce1eaf086f6b77f46dbc2f88a281
Radouane Bahi <radbahi@gmail.com>
2020-02-10 13:41:07 -0500

    createuser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         user = User.all 
         render json: user, include: [:pokemons] # NO SUCH COLUMN POKEMON USERS ID
     end
+
+    def create
+        User.create(params.require(:user.permit(:name)))
+        render json:
+    end
 end
